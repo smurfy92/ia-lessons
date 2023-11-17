3e9641b048878ad8138a3df0a340c9f4936926c1
Leo Grosjean <leo.grosjean@gmail.com>
2023-11-17 23:00:07 +0100

    batch exo 1 read todos.py

---
+++ todos.py
@@ -0,0 +1,19 @@
+# TODO remove me when you read me !
+
+# TODO I added .gitignore from https://github.com/github/gitignore/blob/main/Python.gitignore
+
+# TODO check pandas doc : https://pandas.pydata.org/docs/user_guide/10min.html, skip all plot/chart with pandas
+
+# TODO help(object) to have help on any python object (type, methods, functions...) object? also work instead in jupyter
+
+# TODO read https://www.jeannicholashould.com/tidy-data-in-python.html or
+#  original paper https://vita.had.co.nz/papers/tidy-data.pdf
+
+# TODO execute ./lessons/"Tidy Data.ipynb" you should understand each cells and why we need to tidy the dataset
+#  You should then be able to identify a dirty dataset and know how to clean it
+
+# TODO do notebooks in ./exos (add import, im lazy and forget them)
+
+# TODO call Léo when you're done so we can start scraping
+#  we will use requests https://github.com/psf/requests
+#  and then scrapy https://github.com/scrapy/scrapy
